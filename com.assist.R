af08393924311b9ab9c64baf99b6d13fd1d11eee
Liam Hash <whash@tulane.edu>
2016-07-01 13:00:29 +0200

    Second Commit - goal/assist rough draft

---
+++ com.assist.R
@@ -0,0 +1,24 @@
+com.assist <- function(x = "Assister", y = "Team", z = "Type", a = "Goalscorer", b = "Distance") {
+  str_interp("Beautiful ${z} by ${x} from ${y}, splitting the defenders! Made it very simple for ${a} to put the ball in the net. His vision is incredible, as showcased by that ${b} yard ball.")
+}
+
+com.assist1 <- function(x = "Assister", y = "Team", z = "Type", a = "Goalscorer", b = "Distance") {
+  str_interp("One of the greatest assists you'll ever see! ${x} played a magical ${b} yard ${z} in to the striker, ${a}, to get another goal for ${y} who just seem to be controlling this game.")
+}
+
+com.assist2 <- function(x = "Assister", y = "Team", z = "Type", a = "Goalscorer", b = "Distance") {
+  str_interp("I can't believe the defense just let that happen... they're at the top level of football for God's sake! ${x} just put ${y} up another goal by playing in one of the easiest ${z}s to ${a} from only about ${b} yards away. The playmaker should not have had nearly as much time on the ball - he'll do that to you all day!")
+}
+
+com.assist3 <- function(x = "Assister", y = "Team", z = "Type", a = "Goalscorer", b = "Distance") {
+  str_interp("It's pass mastery out there from ${x} and the whole ${y} team in general; that's why ${x} is one of the most gifted footballers in the world! He played in an incredible ${z} in to ${a} from about ${b} yards, killing off the game for ${y}.")
+}
+
+com.assist4 <- function(x = "Assister", y = "Team", z = "Type", a = "Goalscorer", b = "Distance") {
+  str_interp("Scrappy assist here from ${x} to ${a} for the easiest goal you'll see this year. ${y} picked it up in the opponent's half following some good pressing, and then it was just a simple ${b} yard ${z}")
+}
+
+com.assist5 <- function(x = "Assister", y = "Team", z = "Type", a = "Goalscorer", b = "Distance") {
+  str_interp("No one could have seen that coming! An absolute peach of a ${z} from an unlikely ${x} to ${a} who was ${b} yards away to get ${y} another goal. Unbelievable scenes from inside the stadium!")
+}
+
