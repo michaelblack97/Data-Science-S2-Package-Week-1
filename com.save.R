5c4e6b149621c32084c25eec73fc810bbd4be010
Liam Hash <whash@tulane.edu>
2016-07-01 14:39:05 +0200

    Beginning of saves

---
+++ com.save.R
@@ -0,0 +1,23 @@
+com.save <- function(x = "Goalkeeper", y = "Team", z = "Type", a = "Shooter", b = "Placement") {
+  str_interp("Fantastic ${z} save from the ${y} keeper ${x} to deny ${a} a goal in the ${b} of the goal.")
+}
+
+com.save1 <- function(x = "Goalkeeper", y = "Team", z = "Type", a = "Shooter", b = "Placement") {
+  str_interp("MY WORD IT'S THE SAVE OF THE SEASON! ${x} of ${y} just produced a miraculous ${z} save, one of the best I've ever seen! ${a} was already celebrating as he watched his shot fly towards the ${b} of the goal, only to be denied in spectacular fashion! You'll be seeing highlights of this one for years to come!")
+}
+
+com.save2 <- function(x = "Goalkeeper", y = "Team", z = "Type", a = "Shooter", b = "Placement") {
+  str_interp("Pretty straight forward ${z} save by ${x} to ensure ${y} didn't give up another goal. ${a} didn't do much with the chance, putting it in the ${b} of the goal. Looked like catching practice for ${x}.")
+}
+
+com.save3 <- function(x = "Goalkeeper", y = "Team", z = "Type", a = "Shooter", b = "Placement") {
+  str_interp("Well we've seen this about a million times from ${x}, he just will not let his team concede! Fans of ${y} must be pretty happy that they have this keeper on their side as he just made a spectacular ${z} save from ${a}'s rocket to the ${b} of the goal. The rest of the team is congratulating their keeper on another fine performance today!")
+}
+
+com.save4 <- function(x = "Goalkeeper", y = "Team", z = "Type", a = "Shooter", b = "Placement") {
+  str_interp("Are they ever going to get past ${x}?! He's made another unbelievable ${z} save to keep ${y} from going down a goal here! This is just what he can do when he's in form, and it shows. ${a} placed a beautiful effort towards the ${b} of the goal, but with ${x} performing miracles, it stayed out! You can see ${a}'s frustration as it's just not his day.")
+}
+
+com.save5 <- function(x = "Goalkeeper", y = "Team", z = "Type", a = "Shooter", b = "Placement") {
+  str_interp("Nothing special here from ${y}'s shotstopper, ${x}. Routine ${z} save from a pretty lackluster ${a} effort to the ${b}. With shots like that, ${x} is going to be having a pretty easy day. Let's see if the opposition can start to make any more threatening chances.")
+}
