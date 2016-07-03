1c1a132907158a8b5d78528084ce885ad4aee7d1
Liam Hash <whash@tulane.edu>
2016-07-03 23:00:47 +0200

    Final product...

---
+++ com.card.R
@@ -0,0 +1,27 @@
+com.card <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("Oh and the referee has given a ${z} card to ${y}'s ${x} for that terrible ${a} on the opposing player! What a moment, this could change the way he plays for the rest of the match.")
+}
+
+com.card1 <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("What a terrible ${a} from ${x}! That definitely earns him the ${z} card that the referee looks to be pulling out of his pocket. What on Earth was he thinking with that foul? That's not what we expect from players of his quality. The rest of ${y} look disappointed in his moment of madness. Let's see how this affects the rest of the game.")
+}
+
+com.card2 <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("Oh no and what has the ref done here? It looks like he's giving ${x} a ${z} card! What a terrible call, fans and players of ${y} are absolutely furious out around the stadium! I think I speak for everyone watching when I say that that ${a} was totally fair and did not deserve a card in any sense. This is absolute madness from the referee who really should be making better calls than that. This has changed the landscape of the game!")
+}
+
+com.card3 <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("Well that was just clumsy... The defender ${x} just absolutely stumbled into the opposition's player! That fully deserves the ${z} card the ref is preparing to give him. He should've known not to make that kind of ${a} seeing as he's already made a few fouls earlier in the game. This will be tough for ${y} with one of their star players now on a ${z}.")
+}
+
+com.card4 <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("Oh my he's just asking for that ${z} card!! In the midst of a tense moment on field, ${x} actually knocked heads with the opposing striker! You can NOT do that on the pitch, he really needs to learn to control his anger. It's no wonder the referee is reaching for his pocket now. What was he thinking with that ${a}? Now ${y} has to play with one of their players on a ${z} card which will make this game a lot more difficult.")
+}
+
+com.card5 <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("I cannot believe the call that the ref just made on the ${a} that ${x} just committed! It was one of the cleanest tackles I've ever seen, but the ref had an obscured view and decided to give the player a ${z} card for his troubles. It's no wonder that the whole ${y} team is pleading with the ref on this one, but once the card has been awarded there's no changing it. That blown call has to be one of the stories of the game! Let's just hope the team can cope with one of their stars now on a ${z}.")
+}
+
+com.card6 <- function(x = "Player", y = "Team", z = "Color", a = "Type") {
+  str_interp("Well of course he was going to get the ${z} card for ${a} while celebrating the goal he just scored. It was so unecessary from ${x} to do that, now risking the next few games by being awarded a ${z}. ${y} are still celebrating the fantastic goal though so we'll see how this plays out, if the card makes an impact on the rest of the game.")
+}
